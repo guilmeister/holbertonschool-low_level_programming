388085f36501fc5353303afd0815207fee5c2e9f
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-06-24 18:57:38 +0000

    Added C function that prints the alphabet from a-z x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+/**
+ * print_alphabet_x10 - Prints alphabet a-z x10
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+
+int counter;
+int x;
+
+for (counter = 1; counter <= 10; counter++)
+{
+
+for (x = 97; x <= 122; x++)
+putchar (x);
+
+putchar ('\n');
+
+}
+
+}
