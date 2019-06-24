c10a2de1cd431870e9862341c238373905abc33d
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-06-24 23:52:46 +0000

    Added C Function that tells if it is lowercase or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+B#include "holberton.h"
+
+/**
+ * _islower - Checks for lowercase
+ *
+ * @c: value from main
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+
+if (islower(c))
+return (1);
+
+else
+return (0);
+
+}
