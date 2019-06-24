55631a488a0bd323e6feda368a55210257e664cd
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-06-24 23:34:07 +0000

    Added C Program that prints Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,7 +8,12 @@
 int main(void)
 {
 
-printf("Holberton\n");
+int array[10] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+int x;
 
+for (x = 0; x <= 8; x++)
+_putchar(array[x]);
+
+_putchar('\n');
 return (0);
 }
