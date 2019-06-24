1aece1b73ed22e4c6e75ded7a600a8fab1261168
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-06-24 18:08:41 +0000

    Added C Function that prints the alphabet from a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet - Prints alphabet a-z
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+
+int x;
+
+for (x = 97; x <= 122; x++)
+putchar (x);
+
+putchar ('\n');
+
+}
