26d6c35e7aa6b7d378d45321262b67adf849c7c3
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-06-25 00:04:27 +0000

    Added C Function that tells if its alphanumeric or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Checks for lowercase
+ *
+ * @c: value from main
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+
+
+if (isalpha(c))
+return (1);
+
+else
+return (0);
+
+}
