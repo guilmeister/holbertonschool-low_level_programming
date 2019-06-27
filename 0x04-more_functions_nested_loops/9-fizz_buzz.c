b2c8ee61ef1e1d0dc5d6ce670b81c3c1ea5f45e6
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-06-27 20:35:15 +0000

    Added C Program that prints 1-100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,18 +14,20 @@ int x;
 for (x = 1; x <= 100; x++)
 {
 
-  if (x % 3 == 0 && !(x % 5 == 0))
-printf("Fizz ");
+if (x % 3 == 0)
+printf("Fizz");
 
-  else if (x % 5 == 0 && !(x % 3 == 0))
-printf("Buzz ");
+else if (x % 5 == 0)
+printf("Buzz");
 
 else if (x % 3 == 0 && x % 5 == 0)
-printf("FizzBuzz ");
+printf("FizzBuzz");
 
 else
-printf("%d ", x);
+printf("%d", x);
 
+if (x != 100)
+printf(" ");
 }
 
 printf("\n");
