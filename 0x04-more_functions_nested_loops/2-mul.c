bbf6b0469f094043b83efad1f634199e6f3abb33
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-06-27 16:44:34 +0000

    Added C Function that gives the product of the values given

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies the value given from main
+ *
+ * @a: value from main
+ * @b: value from main
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+
+int x;
+
+x = a * b;
+
+return (x);
+
+}
