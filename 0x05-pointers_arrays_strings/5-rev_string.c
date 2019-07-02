8e702b936966643c14a89e31399f178406a671d2
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-02 01:34:47 +0000

    Added C Function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * rev_string - string in reverse
+ *
+ * @s: value from main
+ *
+ * Return: Always 0 (Success)
+ */
+
+void rev_string(char *s)
+{
+char tmp;
+char *start = s;
+
+while (*s != '\0')
+{
+s++;
+}
+
+start--;
+
+while (start < s)
+{
+start++;
+s--;
+tmp = *start;
+*start = *s;
+*s = tmp;
+}
+
+}
