dd558eda738d094ae634efedeb5e7e05ba0e9f22
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-06 21:46:36 +0000

    Added C Function that copies a string of n bytes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,25 @@
+/**
+ * _strncpy - copies 2 string
+ *
+ * @n: value from main
+ * @dest: value from main
+ * @src: value from main
+ *
+ * Return: value of dest
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 
 	int destcounter;
 
-	for (destcounter = 0; destcounter < n && src[destcounter] != '\0'; destcounter++)
+	for (destcounter = 0; destcounter < n && src[destcounter] != '\0';
+destcounter++)
 	{
 		dest[destcounter] = src[destcounter];
 
-		if (destcounter > n)
-			dest[destcounter] = 0;
+		if (destcounter < n)
+			dest[destcounter + 43] = 0;
 	}
 
 	return (dest);
