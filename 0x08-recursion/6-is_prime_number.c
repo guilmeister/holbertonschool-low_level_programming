7189b3cb95fc1c52bdc229d40bdad3a43f542c27
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-11 00:50:07 +0000

    Added C Function that print primes

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,18 +1,45 @@
 /**
- * is_prime_number - checks for prime numbers
+ * check - checker for prime
+ *
+ * @x: value from main
+ * @checker: value from main
+ *
+ * Return: value negative 1 or 1
+ */
+
+int check(int x, int checker)
+{
+
+	if (x == checker)
+		return (1);
+
+	else if (x % checker != 0)
+		return (check(x, checker + 1));
+
+	else if (x % checker == 0)
+		return (0);
+
+	else
+		return (0);
+}
+
+/**
+ * is_prime_number - checks for prime
  *
  * @n: value from main
  *
- * Return: 1 if prime else 0
+ * Return: value negative 1 or root
  */
 
 int is_prime_number(int n)
 {
 
-	if (n % 1 == 0 && n % n == 0 && n != 1 && n > 0 && n % 2 != 0 && n % 5 != 0)
-		return (1);
-	else
+	int prime;
+
+	if (n < 2)
 		return (0);
 
+	prime = check(n, 2);
 
+	return (prime);
 }
