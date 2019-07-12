36d1b56cbda4744004d8b96fee1ace9f20837d73
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-12 18:29:27 +0000

    Added C Program that adds all arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: counter
+ * @argv: array for stored variables
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) > 0)
+			sum = sum + atoi(argv[i]);
+
+		else if (sizeof(argv[i]) != 4)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+
+	printf("%d\n", sum);
+
+	return (0);
+
+}
