f63417d98afd9be73d5a9e1f7f7f0f9c2f58fcf8
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-12 02:31:53 +0000

    Added C Function that prints diagonal sum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,15 +12,16 @@ void print_diagsums(int *a, int size)
 {
 
 	int x, y;
-	int dsum = 0;
-	int isum = 0;
+	int sum1 = 0;
+	int sum2 = 0;
 	int area = size * size;
 
-	for (x = 0, y = 0; x < area; x = (x + size + 1), y = (y + size - 1))
+	for (x = 0, y = size - 1; x <= area; x = x + size + 1, y = y + size - 1)
 	{
-		dsum = dsum + a[x];
-		isum = isum + a[y];
+		sum1 = sum1 + a[x];
+		sum2 = sum2 + a[y];
 	}
 
-	printf("%d, %d\n", dsum, isum);
+	printf("%d, %d\n", sum1, sum2);
+
 }
