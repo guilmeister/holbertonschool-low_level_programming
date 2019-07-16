bafe8315edda62e96e09b7a3f9fab98b559a35bb
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-16 22:45:29 +0000

    Added C Function that sets 2d array to 0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - prints an array with value set to 0
+ *
+ * @height: y limit
+ * @width: x limit
+ *
+ * Return: buff
+ */
+
+int **alloc_grid(int width, int height)
+{
+
+	int x, y;
+	int **buff;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	buff = (int **) malloc(width * sizeof(int *));
+
+	if (buff == NULL)
+		return (NULL);
+
+	for (x = 0; x < width; x++)
+		buff[x] = (int *) malloc(height * sizeof(int));
+
+	for (x = 0; x < width; x++)
+	{
+		for (y = 0; y < height; y++)
+		{
+			buff[x][y] = 0;
+		}
+
+	}
+
+	return (buff);
+
+}
