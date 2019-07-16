1519aaedf3128d8d7135da4de2b6dd3fe7ec06bd
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-16 21:00:42 +0000

    Added NULL conditions

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ *
+ * @s1: value of main
+ * @s2: value of main
+ *
+ * Return: buff
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+	char *buff;
+	int size = 0;
+	int x, y, z;
+
+	for (z = 0; s1[z] != '\0'; z++)
+		size++;
+
+	for (z = 0; s2[z] != '\0'; z++)
+		size++;
+
+	buff = (char *) malloc((size + 1) * sizeof(char));
+
+	for (x = 0; s1[x] != '\0'; x++)
+		buff[x] = s1[x];
+
+	for (y = 0; s2[y] != '\0'; y++)
+	{
+		buff[x] = s2[y];
+		x++;
+	}
+
+	buff[x] = '\0';
+
+	return (buff);
+
+}
