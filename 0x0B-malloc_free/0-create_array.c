f489b826013673c1e47a24671fae30f9282a3b41
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-16 17:28:21 +0000

    Added C Function that creates an array and initializes it with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates memory allocation for array
+ *
+ * @size: size to allocate
+ * @c: value from main
+ *
+ * Return: buff
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+
+	char *buff = (char *) malloc(size * sizeof(char));
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		buff[i] = c;
+
+	return (buff);
+
+}
