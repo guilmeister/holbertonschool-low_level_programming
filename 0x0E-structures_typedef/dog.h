16884ae6d5477894b73f1147d0136286ad82edb2
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-22 20:47:09 +0000

    Added header file for dogs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _dog_h
+#define _dog_h
+
+/**
+ * struct dog - structure for dog details
+ *
+ * @name: name of dog
+ * @age: how old the dog is
+ * @owner: name of dog owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
