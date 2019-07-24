6c1c4f21b338a01ce75bb459236b1518b4938533
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-24 19:37:28 +0000

    Added include guards

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,9 @@
+#ifndef _function_pointers
+#define _function_pointers
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
