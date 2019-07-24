a895ab71a6c08dd556035b6c21c6cd7cb181b1b0
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-24 20:29:25 +0000

    Added C Functions that does operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+/**
+ * op_add - function that adds two variables given
+ *
+ * @a: value from main
+ * @b: value from main
+ *
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that subtracts two variables given
+ *
+ * @a: value from main
+ * @b: value from main
+ *
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that multiplies two variables given
+ *
+ * @a: value from main
+ * @b: value from main
+ *
+ * Return: product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that divides two variables given
+ *
+ * @a: value from main
+ * @b: value from main
+ *
+ * Return: quotient
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function that modulos two variables given
+ *
+ * @a: value from main
+ * @b: value from main
+ *
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
