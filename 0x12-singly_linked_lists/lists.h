b456bfb7ac6e3954f542e2a02c4744af657ad035
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-08-01 18:13:19 +0000

    Added _strlen prototype and changed data type

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
-#ifndef _LISTS_H
-#define _LISTS_H
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +26,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(const char *s);
+
 
 #endif
