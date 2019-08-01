690732763bd08c12042cf723ca22eb2e84e6e3e7
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-08-01 20:05:12 +0000

    Added NULL checks, and changed return to new

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -34,10 +34,20 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 
+	if (new == NULL)
+		return (NULL);
+
+	if (str == NULL)
+		return (NULL);
+
 	new->str = strdup(str);
+
+	if (new->str == NULL)
+		return (NULL);
+
 	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
 
-	return (0);
+	return (new);
 }
