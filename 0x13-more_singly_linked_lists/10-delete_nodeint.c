ba2518a22baa631620f661329d04b60d2a8ba51f
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-08-06 15:41:50 +0000

    Added NULL condition at top

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temporary;
 	listint_t *next;
 
-	if (*head == NULL)
+	if ((*head) == NULL)
+		return (-1);
+
+	if (head == NULL)
 		return (-1);
 
 	temporary = *head;
