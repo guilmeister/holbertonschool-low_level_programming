114afc7aae01ba06e0ac689d1e2244d964150128
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-08-06 15:33:20 +0000

    Added if head is NULL condition at top

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,7 +14,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temporary;
 
-	while (*head != NULL)
+	if ((*head) == NULL)
+		return;
+
+	while ((*head) != NULL)
 	{
 		temporary = *head;
 		(*head) = (*head)->next;
