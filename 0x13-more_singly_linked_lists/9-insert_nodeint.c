15704f737de10b8954d7096a07b783a35eba500c
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-08-06 16:50:28 +0000

    Added actual length function and prototype at top

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+size_t listint_len(const listint_t *h);
+
+/**
+ * listint_len - print the values inside the list given
+ *
+ * @h: value passed main
+ *
+ * Return: count
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
 
 /**
  * insert_nodeint_at_index - add new node at index
