3e56e8bf9007e73fcef55b891a87574705e12d79
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-08-06 18:11:22 +0000

    Added if idx == 0 condition and insert value

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,28 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t listint_len(const listint_t *h);
-
-/**
- * listint_len - print the values inside the list given
- *
- * @h: value passed main
- *
- * Return: count
- */
-
-size_t listint_len(const listint_t *h)
-{
-	int count = 0;
-
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
 /**
  * insert_nodeint_at_index - add new node at index
  *
@@ -38,14 +16,8 @@ size_t listint_len(const listint_t *h)
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int x;
-	unsigned int listlength = 0;
 	listint_t *new;
 	listint_t *temporary = *head;
-
-	listlength = listint_len(*head);
-
-	if (idx < 1 || idx > listlength + 1)
-		return (NULL);
 
 	if (head == NULL || (*head) == NULL)
 		return (NULL);
@@ -55,10 +27,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
+	if (idx == 0)
+	{
+		new->next = (*head);
+		(*head) = new;
+		(*head)->n = n;
+		return (*head);
+	}
+
 	for (x = 0; x < idx - 1 && temporary != NULL; x++)
 		temporary = temporary->next;
 
-	if (temporary == NULL || temporary->next == NULL)
+	if (temporary == NULL)
 	{	free(new);
 		return (NULL);
 	}
