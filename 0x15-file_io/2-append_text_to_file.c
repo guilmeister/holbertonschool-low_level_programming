3a84d3563d710853d2c0f7a53107390ae986835c
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-08-13 23:16:47 +0000

    Changed flags

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -43,7 +43,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	buffer = open(filename, O_WRONLY | O_APPEND, 0600);
+	buffer = open(filename, O_RDWR | O_APPEND, 0600);
 
 	if (buffer == -1)
 		return (-1);
