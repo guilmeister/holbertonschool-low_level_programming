70856132b9b4a0c5b81085955b826bba7a7a0d15
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-08-14 01:53:47 +0000

    Added edge cases for exit 97-100

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,34 +17,40 @@
 
 int main(int ac, char **av)
 {
-	int filedesc;
+	int filedesc, wchecker, cchecker;
 	int actualsize = 0;
 	char array[1024];
 	char *err97 = "Usage: cp file_from file_to\n";
-/*	char *err98 = "Error: Can't read from file NAME_OF_THE_FILE";
-	char *err99 = "Error: Can't write to \n";*/
-	char *err100 = "Error: Can't close fd FD_VALUE\n";
 
 	if (ac != 3)
 	{	write(STDERR_FILENO, err97, 29);
-		exit (97);
+		exit(97);
 	}
 	filedesc = open(av[1], O_RDONLY);
 	if (filedesc == -1)
-	{	write(STDERR_FILENO, err100, 32);
-		exit (100);
+	{	dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
 	}
 	actualsize = read(filedesc, array, 1024);
-	close(filedesc);
-
-	filedesc = open(av[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
-
-	if (filedesc == -1)
-	{	write(STDERR_FILENO, err100, 32);
-		exit (100);
+	cchecker = close(filedesc);
+	if (cchecker == -1)
+	{	dprintf(2, "Error: Can't close fd %d", cchecker);
+		exit(100);
 	}
-	write(filedesc, array, actualsize);
-
-	close(filedesc);
+	filedesc = open(av[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
+	if (filedesc == -1)
+	{	dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	wchecker = write(filedesc, array, actualsize);
+	if (wchecker == -1)
+	{	dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	cchecker = close(filedesc);
+	if (cchecker == -1)
+	{	dprintf(2, "Error: Can't close fd %d\n", cchecker);
+		exit(100);
+	}
 	return (0);
 }
