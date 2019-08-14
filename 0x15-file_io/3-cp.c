30382f9d77ca3e14fd758fa2a1b7561592c8bdeb
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-08-14 20:43:42 +0000

    changed 2 to err macro

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,40 +22,39 @@ int main(int ac, char **av)
 	int filedesc1, filedesc2, wchecker, cchecker;
 	int actualsize = 0;
 	char array[1024];
-	char *err97 = "Usage: cp file_from file_to";
 
 	if (ac != 3)
-	{	write(STDERR_FILENO, err97, 28);
+	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);	}
 	filedesc1 = open(av[1], O_RDONLY);
 	if (filedesc1 == -1)
-	{	dprintf(2, "Error: Can't read from file %s\n", av[1]);
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);	}
 	filedesc2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (filedesc2 == -1)
-	{	dprintf(2, "Error: Can't write to %s\n", av[2]);
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);	}
 	while ((actualsize = read(filedesc1, array, 1024)))
 	{
 		if (actualsize == -1)
-		{	dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			closerfunction(filedesc1);
 			closerfunction(filedesc2);
 			exit(98);		}
 		wchecker = write(filedesc2, array, actualsize);
 		if (wchecker == -1)
-		{	dprintf(2, "Error: Can't write to %s\n", av[2]);
+		{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			closerfunction(filedesc1);
 			closerfunction(filedesc2);
 			exit(99);		}
 	}
 	cchecker = close(filedesc1);
 	if (cchecker == -1)
-	{	dprintf(2, "Error: Can't close fd %d\n", cchecker);
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cchecker);
 		exit(100);	}
 	cchecker = close(filedesc2);
 	if (cchecker == -1)
-	{	dprintf(2, "Error: Can't close fd %d\n", cchecker);
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cchecker);
 		exit(100);	}
 	return (0);
 }
@@ -72,5 +71,5 @@ void closerfunction(int filename)
 	int cchecker = close(filename);
 
 	if (cchecker == -1)
-		dprintf(2, "Error: Can't close fd %d\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filename);
 }
