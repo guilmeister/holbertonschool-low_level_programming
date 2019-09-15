ac160aeb86ba1fdc691a335d0ae33fe5340f70c9
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-15 00:24:33 +0000

    Added case where head is NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,12 +11,22 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
 
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
+	if ((*head) == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
 	new->next = *head;
 	*head = new;
 
