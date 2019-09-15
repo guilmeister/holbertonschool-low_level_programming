86ab2d43d64b69a5d1b0aa05b5c75e25d2351c84
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-15 05:36:05 +0000

    Reverted to previous code and betty formatting fixes

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,26 +14,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int x;
 	dlistint_t *new;
-	dlistint_t *last = *h;
 	dlistint_t *temporary = *h;
 	dlistint_t *store;
 
 	if (idx != 0 && (h == NULL || (*h) == NULL))
 		return (NULL);
-
 	if (idx == 0 && (h == NULL || (*h) == NULL))
-	{
-		new = add_dnodeint(h, n);
+	{	new = add_dnodeint(h, n);
 		return (new);
 	}
-	while (last != NULL)
-		last = last->next;
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	if (idx == 0 && (h != NULL || (*h) != NULL))
-	{
-		new->next = (*h);
+	{	new->next = (*h);
 		(*h) = new;
 		(*h)->prev = NULL;
 		(*h)->n = n;
@@ -42,14 +36,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	for (x = 0; x < idx - 1 && temporary != NULL; x++)
 		temporary = temporary->next;
 	if (temporary == NULL)
-	{
-		free(new);
+	{	free(new);
 		return (NULL);
-	}
-	if (temporary == last)
-	{
-		new = add_dnodeint_end(h, n);
-		return (new);
 	}
 	store = temporary->next;
 	temporary->next = new;
