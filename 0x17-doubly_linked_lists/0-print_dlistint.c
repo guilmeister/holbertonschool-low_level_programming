b43719e7c7e379e84f1972c86aa39eb9117ebd7e
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-15 00:00:46 +0000

    Fixed syntax errors, mismatched prototype names

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,16 +8,13 @@
  * Return: count
  */
 
-size_t print_dlistint(const list_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
