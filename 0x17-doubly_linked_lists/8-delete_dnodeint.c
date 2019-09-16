13f534391fcfcfd867c226f9b0a261e7d4609921
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-16 04:31:30 +0000

    Added pointers to previous on first node delete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -23,6 +23,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = temporary->next;
+		if ((*head) != NULL)
+			(*head)->prev = NULL;
 		free(temporary);
 		return (1);
 	}
