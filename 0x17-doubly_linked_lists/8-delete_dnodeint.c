f72cd5754b801b96865dc27581857cc485ad4b06
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-09-16 04:41:17 +0000

    Added case for end node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,32 +12,35 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int x;
-	dlistint_t *temporary;
+	dlistint_t *temporary, *last;
 	dlistint_t *next;
 
 	if ((*head) == NULL || head == NULL)
 		return (-1);
-
 	temporary = *head;
-
+	last = *head;
 	if (index == 0)
-	{
-		*head = temporary->next;
+	{	*head = temporary->next;
 		if ((*head) != NULL)
 			(*head)->prev = NULL;
 		free(temporary);
 		return (1);
 	}
-
+	while (last->next != NULL)
+		last = last->next;
 	for (x = 0; x < index - 1 && temporary != NULL; x++)
 		temporary = temporary->next;
-
 	if (temporary == NULL || temporary->next == NULL)
 		return (-1);
-
+	if (temporary == last)
+	{	*head = temporary->prev;
+		if ((*head) != NULL)
+			(*head)->next = NULL;
+		free(temporary);
+		return (1);
+	}
 	next = temporary->next->next;
 	free(temporary->next);
 	temporary->next = next;
-
 	return (1);
 }
