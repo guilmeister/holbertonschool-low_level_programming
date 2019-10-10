0d61b80a4b5cca2606607d8258a7df2d4d0e8a3d
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-10 23:21:11 +0000

    betty fixes

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - implements the djb2 algorithm
+ * @str: string passed in main
+ * Return: hash
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
@@ -8,7 +14,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	hash = 5381;
 	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		hash = ((hash << 5) + hash) + c;
 	}
 	return (hash);
 }
