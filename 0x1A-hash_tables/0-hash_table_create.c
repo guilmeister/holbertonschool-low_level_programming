7123ca6b6552ce6df48333461fbea7d2a1843e0c
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-10 23:00:10 +0000

    Added files for header, djb2 function, empty hash table creation

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size for array passed in main
+ * Return: new hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *array;
+	hash_table_t *hash_table;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	array = malloc(size * sizeof(hash_table));
+	if (array == NULL)
+		return (NULL);
+
+	return (hash_table);
+}
