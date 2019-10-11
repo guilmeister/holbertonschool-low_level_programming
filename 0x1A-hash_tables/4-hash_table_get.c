f87ba441c5e9c4addb9a935139d2098d8a6b2684
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-11 20:59:03 +0000

    added check if hash passed is null

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *new;
 
+	if (ht == NULL)
+		return (NULL);
 	idx = key_index((const unsigned char *) key, ht->size);
 	new = ht->array[idx];
 
