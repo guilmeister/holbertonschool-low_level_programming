63a9ad8d8d280a8e9368ddfb9b303a333df33436
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-11 02:16:30 +0000

    Set struct size to value of size given

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
-	hash_table->array = malloc(size * sizeof(hash_table_t **));
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_table_t **));
 	if (hash_table->array == NULL)
 		return (NULL);
 
