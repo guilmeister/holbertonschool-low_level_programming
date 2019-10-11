aa9ff54d63f8c1a7563b7839c01e37fba17281bf
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-11 20:44:12 +0000

    changed to check for keys with same value, update the keys, duplication of key and values

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,34 +10,62 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *new;
+
+	if (ht == NULL)
+		return (0);
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	node_finder(ht, new, key);
+
+	return (1);
+}
+
+/**
+ * node_finder - find the value inside a linked list inside an array
+ * @ht: hash table passed
+ * @key: value string passed from hash table function
+ * @head: node passed malloc'd from hash table function
+ */
+
+void node_finder(hash_table_t *ht, hash_node_t *head, const char *key)
+{
 	unsigned long int idx;
-	hash_node_t *head;
-	char *dup_key, *dup_value;
+	hash_node_t *new;
 
 	idx = key_index((const unsigned char *) key, ht->size);
-	if (ht->array[idx] == NULL)
+	new = ht->array[idx];
+	if (ht->array[idx] != NULL)
 	{
-		head = malloc(sizeof(ht->array[idx]));
-		if (head == NULL)
-			return (0);
-		head->next = ht->array[idx];
-		ht->array[idx] = head;
-		dup_key = strdup(key);
-		dup_value = strdup(value);
-		head->key = dup_key;
-		head->value = dup_value;
-		return (1);
+		new = ht->array[idx];
+		while (new != NULL)
+		{
+			if (strcmp(new->key, head->key) == 0)
+				break;
+			new = new->next;
+		}
+		if (new == NULL)
+		{
+			head->next = ht->array[idx];
+			ht->array[idx] = head;
+		}
+		else
+		{
+			free(new->value);
+			new->value = strdup(head->value);
+			free(head->value);
+			free(head->key);
+			free(head);
+		}
 	}
 	else
 	{
-		head = malloc(sizeof(ht->array[idx]));
-		if (head == NULL)
-			return (0);
+		head->next = NULL;
 		ht->array[idx] = head;
-		dup_key = strdup(key);
-		dup_value = strdup(value);
-		head->key = dup_key;
-		head->value = dup_value;
-		return (1);
 	}
 }
