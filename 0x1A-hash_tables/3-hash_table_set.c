9dd07301f310646172a90517b9ed58a6fe1074f5
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-11 02:17:20 +0000

    Added pointer to next

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,8 +3,8 @@
 /**
  * hash_table_set - insert new element in the hash table
  * @ht: hash table passed
- * @value: value for array passed in main
- * @size: size for array passed in main
+ * @key: value string passed in main
+ * @value: value string passed in main
  * Return: 1/0 if it succeeded/failed
  */
 
@@ -16,10 +16,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	idx = key_index((const unsigned char *) key, ht->size);
 	head = malloc(sizeof(ht->array[idx]));
+	head->next = ht->array[idx];
+	ht->array[idx] = head;
 	dup_key = strdup(key);
 	dup_value = strdup(value);
 	head->key = dup_key;
 	head->value = dup_value;
-
 	return (1);
 }
