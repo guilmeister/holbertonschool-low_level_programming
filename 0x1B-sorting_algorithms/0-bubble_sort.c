8e5ef4d0ca4ee0bd08b5b316ac0ec23eb873ecc6
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-28 15:37:30 +0000

    betty compliant 0

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -11,18 +11,6 @@ void bubble_sort(int *array, size_t size)
 	size_t x, y;
 	int flag = 0, swap;
 
-/*	for (y = 0; y < size; y++)
-	{
-		if (array[y] > array[y + 1])
-		{
-			swap = array[y];
-			array[y] = array[y + 1];
-			array[y + 1] = swap;
-		}
-		print_array(array, size);
-	}
-*/
-
 	for (x = 0; x < size; x++)
 	{
 		for (y = 0; y < size - x - 1; y++)
