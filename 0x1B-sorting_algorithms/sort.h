8694f0f59f7158f54f754fd8f2e48c31cc78a3ee
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-28 21:33:20 +0000

    added updated header file

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -26,5 +26,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void recursive_quick_sort(int *array, int first, int last, size_t size);
+int lomuto_partition(int *array, int first, int last, size_t size);
 
 #endif
