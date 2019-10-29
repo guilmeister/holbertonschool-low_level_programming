51b3dfb6f99daa839333ab0f3222e5e491ea8740
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-29 03:10:42 +0000

    added working insertion

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -7,21 +7,26 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current = *list, *current_next = *list;
 
-	listint_t *sort;
-	listint_t *next;
-	listint_t *current = *head;
-
-	while (current != NULL)
+	while (current_next != NULL)
 	{
-		next = current->next;
-
-		while (current->next != NULL && current->next->n < current->n)
+		current_next = current_next->next;
+		while (current->prev != NULL && current->n < current->prev->n)
 		{
-			current = current->next;
+			current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+			current->next = current->prev;
+			current->prev = current->next->prev;
+			current->next->prev = current;
+			if (current->prev == NULL)
+				*list = current;
+			else
+				current->prev->next = current;
+			print_list(*list);
 		}
-		
-		current = next;
+		current = current_next;
 	}
 
 }
