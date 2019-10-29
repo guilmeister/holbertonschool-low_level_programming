b3e07b45e56a0a1f0d4435487c624c33afd18a62
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-10-29 06:32:38 +0000

    changed if statement

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -18,7 +18,7 @@ void insertion_sort_list(listint_t **list)
 		while (current->prev != NULL && current->n < current->prev->n)
 		{
 			current->prev->next = current->next;
-			if (current->next != NULL)
+			if (current->next)
 				current->next->prev = current->prev;
 			current->next = current->prev;
 			current->prev = current->next->prev;
