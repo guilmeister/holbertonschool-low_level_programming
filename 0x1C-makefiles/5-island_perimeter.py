3e08b8cf7d0db94cb75d933eeee613d144c4caf9
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-16 01:00:16 +0000

    pep8 compliant

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,25 +3,26 @@
 Module to find perimeter
 """
 
+
 def island_perimeter(grid):
     """
     Function to get the perimeter of an island
     """
     perimeter = 0
-    if grid is None:
+    if not grid:
         return perimeter
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
-                directions = [ [0, 1], [1, 0], [0, -1], [-1, 0] ]
+                directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
                 for z in range(len(directions)):
                     iterx = x + directions[z][0]
                     itery = y + directions[z][1]
                     if (0 <= iterx < len(grid) and
-                        0 <= itery < len(grid[x]) and
-                        grid[iterx][itery] == 0):
+                       0 <= itery < len(grid[x]) and
+                       grid[iterx][itery] == 0):
                         perimeter = perimeter + 1
-                    elif (iterx == -1 or iter == len(grid)
-                          or itery == -1 or itery == len(grid[x])):
+                    elif (iterx == -1 or iter == len(grid) or itery == -1 or
+                          itery == len(grid[x])):
                         perimeter = perimeter + 1
     return perimeter
