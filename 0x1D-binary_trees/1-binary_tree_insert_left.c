2231920571a92cc8d55ae0dc73e8f4a5fc84dd7a
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-12-04 22:16:46 +0000

    added 1

---
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function that inserts a node as the left-child
+ * @parent: pointer to parent node
+ * @value: value put in new node
+ * Return: New node created
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+	new->parent = parent;
+	new->n = value;
+
+	if (parent->left)
+	{
+		new->left = parent->left;
+		new->left->parent = new;
+	}
+	parent->left = new;
+	return (new);
+}
