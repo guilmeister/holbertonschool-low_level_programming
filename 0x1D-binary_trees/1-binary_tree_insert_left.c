dc0800f5845c59e467ed95dcb00ee78c9647360e
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-12-04 23:33:06 +0000

    added cond

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new->parent = parent;
 	new->n = value;
 
-	if (parent->left)
+	if (parent->left != NULL)
 	{
 		new->left = parent->left;
 		new->left->parent = new;
--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new->parent = parent;
 	new->n = value;
 
-	if (parent->right)
+	if (parent->right != NULL)
 	{
 		new->right = parent->right;
 		new->right->parent = new;
