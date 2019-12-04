7813ed8dce20418e80994009fd3e545b0ccec54f
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-12-04 22:20:42 +0000

    changed malloc to call function that creates node

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	new = malloc(sizeof(binary_tree_t));
+	new = binary_tree_node(parent, value);
 	if (new == NULL)
 		return (NULL);
 	new->parent = parent;
