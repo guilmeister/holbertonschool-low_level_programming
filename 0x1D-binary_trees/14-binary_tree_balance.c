344b97599eea3d3deba4db926f965d80cdb6392d
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-12-05 00:07:52 +0000

    added check if there are no children

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -30,7 +30,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int left_height = 0, right_height = 0;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
 	else
